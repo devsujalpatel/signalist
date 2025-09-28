27a7b21c683f33095585929072bebd6bd0294474
devsujalpatel <sujal7455@gmail.com>
2025-09-28 17:12:40 +0530

    layout syle

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,7 +1,7 @@
-import Image from "next/image";
 import Link from "next/link";
+import Image from "next/image";
 
-const Layout = ({ children }: { children: React.ReactNode }) => {
+const Layout = async ({ children }: { children: React.ReactNode }) => {
   return (
     <main className="auth-layout">
       <section className="auth-left-section scrollbar-hide-default">
@@ -10,23 +10,51 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
             src="/assets/icons/logo.svg"
             alt="Signalist logo"
             width={140}
-            height={140}
-            className="h-8 w-auto cursor-pointer"
+            height={32}
+            className="h-8 w-auto"
           />
         </Link>
 
         <div className="pb-6 lg:pb-8 flex-1">{children}</div>
       </section>
+
       <section className="auth-right-section">
-        <div className="z-10 relative lg:mt-4 lg:mb-15">
+        <div className="z-10 relative lg:mt-4 lg:mb-16">
           <blockquote className="auth-blockquote">
             Signalist turned my watchlist into a winning list. The alerts are
             spot-on, and I feel more confident making moves in the market
           </blockquote>
+          <div className="flex items-center justify-between">
+            <div>
+              <cite className="auth-testimonial-author">- Ethan R.</cite>
+              <p className="max-md:text-xs text-gray-500">Retail Investor</p>
+            </div>
+            <div className="flex items-center gap-0.5">
+              {[1, 2, 3, 4, 5].map((star) => (
+                <Image
+                  src="/assets/icons/star.svg"
+                  alt="Star"
+                  key={star}
+                  width={20}
+                  height={20}
+                  className="w-5 h-5"
+                />
+              ))}
+            </div>
+          </div>
+        </div>
+
+        <div className="flex-1 relative">
+          <Image
+            src="/assets/images/dashboard.png"
+            alt="Dashboard Preview"
+            width={1440}
+            height={1150}
+            className="auth-dashboard-preview absolute top-0"
+          />
         </div>
       </section>
     </main>
   );
 };
-
 export default Layout;
