47647680cc74d5df10d966ae0b1eae0350d837ca
devsujalpatel <sujal7455@gmail.com>
2025-09-29 01:02:16 +0530

    inngest setup

---
+++ lib/inngest/functions.ts
@@ -0,0 +1,43 @@
+import { inngest } from "./client";
+import { PERSONALIZED_WELCOME_EMAIL_PROMPT } from "./prompts";
+
+export const sendSignUpEmail = inngest.createFunction(
+  { id: "sign-up-email" },
+  { event: "app/user.created" },
+  async ({ event, step }) => {
+    const userProfile = `
+        - Country: ${event.data.country}
+        - Investment goals: ${event.data.investmentGoals}
+        - Risk tolerance: ${event.data.riskTolerance}
+        - Preferred industry: ${event.data.preferredIndustry}
+        `;
+
+    const prompt = PERSONALIZED_WELCOME_EMAIL_PROMPT.replace(
+      `{{userProfile}}`,
+      userProfile
+    );
+
+    const response = await step.ai.infer(`generate-welcome-intro`, {
+      model: step.ai.models.gemini({ model: "gemini-2.5-flash-lite" }),
+      body: {
+        contents: [
+          {
+            role: "user",
+            parts: [{ text: prompt }],
+          },
+        ],
+      },
+    });
+    await step.run("send-welcome-email", async () => {
+      const part = response.candidates?.[0]?.content?.parts?.[0];
+      const introText =
+        (part && "text" in part ? part.text : "") ||
+        "Thanks for joining Signalist. You now have the tools to track market and make smarter move ";
+      // Email sending logic
+    });
+    return {
+      success: true,
+      message: "Welcome Email sent successfully",
+    };
+  }
+);
